1ec2aadfd9114b456193cd3b5787731ff7a56880
Kletss <kletss@mail.ru>
2022-05-03 21:04:56 +0300

    LaunchScreen

---
+++ HydrangeaGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HydrangeaGB
+//
+//  Created by KKK on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
